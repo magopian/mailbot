f363d27058802ef693290b6bc54daa3262d7ac07
Mathieu Agopian <mathieu.agopian@gmail.com>
2013-03-20 09:06:35 +0100

    release 0.1

--- setup.py
+++ setup.py
@@ -11,18 +11,21 @@ def read_relative_file(filename):
         return f.read()
 
 
+LONG_DESC = "%s\r\n\r\n%s" % (read_relative_file('README.rst'),
+                              read_relative_file('CHANGELOG'))
+
+
 if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
     setup(name='mailbot',
           version=read_relative_file('VERSION').strip(),
           description="MailBot: execute callback on mail reception",
-          long_description=read_relative_file('README.rst'),
+          long_description=LONG_DESC,
           classifiers=['Development Status :: 3 - Alpha',
                        'Environment :: Console',
                        'Intended Audience :: Developers',
                        'License :: OSI Approved :: BSD License',
                        'Natural Language :: English',
-                       'Programming Language :: Python',
-          ],
+                       'Programming Language :: Python'],
           keywords='mail callback',
           author='Mathieu Agopian',
           author_email='mathieu.agopian@gmail.com',
